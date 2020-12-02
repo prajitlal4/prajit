906a0f4bbfedbebacc75a7d694e85651759cfe21
Prajit Lal <prajit.lal4@gmail.com>
2020-12-02 13:48:44 +0800

    progress

---
+++ src/pages/contact.js
@@ -0,0 +1,15 @@
+import React from "react"
+import Layout from "../components/layout"
+
+export default function About() {
+    return (
+        <Layout>
+            <h1>
+                Contact
+            </h1>
+            <p>
+                Contact me!
+            </p>
+        </Layout>
+    )
+}
